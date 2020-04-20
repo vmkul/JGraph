7bda6bb6d40560a9c7e841609d37ced85a885588
vmkul <55065764+vmkul@users.noreply.github.com>
2020-04-21 02:31:02 +0300

    Files for lab4

---
+++ lab4.js
@@ -0,0 +1,61 @@
+const test = [
+  [0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 1, 0],
+  [1, 1, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 1, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0],
+];
+
+let arr, ctx, graph1, last, alert, k = 1;
+
+const draw = () => {
+  const canvas1 = document.getElementById('graph1');
+  const canvas2 = document.getElementById('tree');
+  alert = document.getElementById('alert');
+
+  if (canvas1.getContext && canvas2.getContext) {
+    const container1 = document.getElementById('graph1_info');
+    ctx = canvas1.getContext('2d');
+    const ctx2 = canvas2.getContext('2d');
+    graph1 = new Graph(ctx, test, true, 20);
+    const data = BFS(graph1, 6);
+    const tree = data.tree;
+    arr = data.arr;
+    const graph2 = new Graph(ctx2, tree, true, 10);
+
+    graph1.circle(400, 500, 500);
+    graph1.info(container1);
+    graph2.circle(100, 200, 200);
+    step();
+  }
+};
+
+const step = () => {
+  if (k > test.length) {
+    alert.style.display = 'block';
+    return;
+  }
+
+  if (typeof(last) == 'number') {
+    ctx.restore();
+    ctx.clearRect(0, 0, 1000, 1000);
+    graph1.circle(400, 500, 500);
+  }
+
+  const curr = arr.indexOf(k);
+  ctx.save();
+  ctx.strokeStyle = 'green';
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(graph1.vertices[curr].x, graph1.vertices[curr].y, 40, 0, 2 * Math.PI);
+  ctx.stroke();
+  k++;
+  last = curr;
+};
+
+const reset = () => {
+  alert.style.display = 'none';
+  k = 1;
+  step();
+};
